38d01fceb22138e471ab17628ccdb52a77090a12
Aishwary Narayan <101210224+Aishwary21@users.noreply.github.com>
2023-11-07 13:19:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+gsap.from("#background", { duration: 1, x: 200, opacity: 0, ease: "power2.out", delay: 0.6 });
+gsap.from("#achievements", { duration: 1, x: 400, opacity: 0, ease: "power2.out", delay: 0.8 });
+gsap.from("#quotes", { duration: 1, x: 600, opacity: 0, ease: "power2.out", delay: 1.0 });
